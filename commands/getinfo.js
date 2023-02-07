6c314ef6064c4db31620dc4c8e578af7405fb841
tfw_e <carloflores953@gmail.com>
2023-02-06 20:31:01 -0500

    added more information to getinfo, made it slightly nicer to look at

--- commands/getinfo.js
+++ commands/getinfo.js
@@ -1,5 +1,8 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { Player } = require('../dbinit.js');
+const { Player, Match } = require('../dbinit.js');
+const { Sequelize } = require('sequelize');
+const Op = Sequelize.Op;
+
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -17,18 +20,55 @@ module.exports = {
     if (!player) {
       return interaction.reply('Player not found.');
     }
-    const inMatch = (player.matchid !== 'N/A') ? true : false
+    const totalPlayers = await Player.count();
+    const playersWithHigherElo = await Player.count({ where: { elo: { [Op.gte]: player.elo } } });
+    const percentage = (playersWithHigherElo / totalPlayers) * 100;
+    const highestPercentage = 100 - percentage;
+    const inMatch = (player.matchid !== 'N/A')
+    const matchCount = await Match.count({
+      where: {
+        [Op.or]: [
+          { player1id: player.userid, winner: { [Op.ne]: 'N/A' } },
+          { player2id: player.userid, winner: { [Op.ne]: 'N/A' } },
+        ],
+      },
+    })
+    const winsCount = await Match.count({
+      where: {
+        [Op.or]: [
+          { player1id: player.userid, winner: player.userid },
+          { player2id: player.userid, winner: player.userid },
+        ],
+      },
+    });
+    const regionPlayers = await Player.findAll({
+      where: { region: player.region },
+      order: [['elo', 'DESC']],
+    });
+
+    const regionPlayerIndex = regionPlayers.findIndex(p => p.userid === player.userid);
+    const regionPlayerRank = regionPlayerIndex + 1;
+    const regionTotalPlayers = regionPlayers.length;
     const playerInfo = `
 \`\`\`Player Information:
 -----------------
 Handle: ${player.handle}
 Region: ${player.region}
 ELO: ${player.elo}
-Disputes: ${player.disputes}
+Matches played: ${matchCount}
+Matches won: ${winsCount}
+Win rate: ${(winsCount / matchCount * 100).toFixed(2)}%
+Rank in ${player.region}: ${regionPlayerRank}/${regionTotalPlayers}
+
 In a match: ${inMatch}
-Created At: ${player.createdAt}
-Updated At: ${player.updatedAt}\`\`\``;    
+Disputes: ${player.disputes}
+Created At: ${player.createdAt.toLocaleDateString()}
+Updated At: ${player.updatedAt.toLocaleDateString()}
+
+You are in the top ${highestPercentage.toFixed(2)}% of players! (#${playersWithHigherElo} / ${totalPlayers})
+    \`\`\``;
+
     return interaction.reply(`${playerInfo}`);
+
   },
 };
-
